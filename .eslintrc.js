aa90bed27d6dbcbe7c53b9851ee884f68650afe5
刘天青 <liutq@hollycrm.com>
2021-11-19 14:41:16 +0800

    chore: add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'semi': 'off',
+    'no-console': 'off',
     'comma-dangle': 'off',
     'space-before-function-paren': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
